c8e7d4922d7c4aa576fd26dedcd780b3807c7da5
hiroki.sakonju <hiroki.sakonju@broadleaf.co.jp>
2018-07-23 19:39:22 +0900

    initial commit

---
+++ iptables_checker.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("/proc/net/ip_tables_names")
+	if err != nil {
+		defer file.Close()
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "NG")
+		return
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "filter" {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "OK")
+			return
+		} else {
+			continue
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "NG")
+}
+
+func main() {
+	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+	http.ListenAndServe(":80", nil)
+}
